2111c9f8b7d262fd42e51f56e8ef12445dfb8c04
anton-danylov <anton-danylov@users.noreply.github.com>
2018-04-09 23:12:32 +0200

    rules added

--- ShoppingCheckout/ShoppingCheckout.Tests/CheckoutTests.cs
+++ ShoppingCheckout/ShoppingCheckout.Tests/CheckoutTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace ShoppingCheckout.Tests
@@ -8,11 +9,40 @@ namespace ShoppingCheckout.Tests
         [Fact]
         public void Empty()
         {
-            ShoppingCart cart = new ShoppingCart();
+            ShoppingCart cart = new ShoppingCart(null);
 
             decimal total = cart.GetTotal();
 
             Assert.Equal(.0m, total);
+        }
+
+        [Fact]
+        public void SimpleSum()
+        {
+            ShoppingCart cart = new ShoppingCart(null);
+
+            cart.Add(new Product("TypeA", 10.0m));
+            cart.Add(new Product("TypeB", 20.0m));
+
+            decimal total = cart.GetTotal();
+            Assert.Equal(30.0m, total);
+        }
+
+        [Fact]
+        public void DiscountOn3for2()
+        {
+            List<ICheckoutRule> rules = new List<ICheckoutRule>();
+            rules.Add(new Discount3for2Rule("TypeA"));
+
+            ShoppingCart cart = new ShoppingCart(rules);
+
+            cart.Add(new Product("TypeA", 10.0m));
+            cart.Add(new Product("TypeA", 10.0m));
+            cart.Add(new Product("TypeA", 10.0m));
+
+
+            decimal total = cart.GetTotal();
+            Assert.Equal(20.0m, total);
         }
     }
 }
--- ShoppingCheckout/ShoppingCheckout/ShoppingCart.cs
+++ ShoppingCheckout/ShoppingCheckout/ShoppingCart.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace ShoppingCheckout
 {
     public class ShoppingCart
     {
+        private List<Product> _products = new List<Product>();
+        private readonly IEnumerable<ICheckoutRule> _rules;
+
+        public ShoppingCart(IEnumerable<ICheckoutRule> rules)
+        {
+            _rules = rules;
+        }
         public decimal GetTotal()
         {
-            return .0m;
+            return _products.Sum(p => p.Price) - CalculateDiscount();
+        }
+
+
+        private decimal CalculateDiscount()
+        {
+            if (_rules == null)
+            {
+                return .0m;
+            }
+
+            decimal totalDiscount = .0m;
+            
+            foreach(var rule in _rules)
+            {
+                decimal currentDiscount = rule.Apply(_products);
+                totalDiscount += currentDiscount;
+            }
+
+            return totalDiscount;
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
         }
     }
 }
